9348568a5d7c6d56308b6c46012042c66523eada
zinyatou <127304542+zinyatou@users.noreply.github.com>
2023-07-19 04:01:39 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import AddTask from './AddTask';
+import ListTask from './ListTask';
+
+const App = () => {
+  return (
+    <div>
+      <AddTask />
+      <ListTask />
+    </div>
+  );
+};
+
+export default App;
